20cb38c5b782f116e379d7dcfad6b9e924fb88ba
Code <code1832389863@gmail.com>
2020-07-01 23:57:18 +0800

    four

--- Jango/urls.py
+++ Jango/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include(('app.urls', 'app'), namespace='app'))
+    path('app/', include(('app.urls', 'app'), namespace='app')),
+    path('two/', include(('Two.urls', 'Two'), namespace='two')),
+    path('three/', include(('three.urls', 'three'), namespace='three')),
 ]
